df7998d47fd52e36c6f7e16bb3a732464b9ce23e
astridovic <148537371+asturdovic@users.noreply.github.com>
2024-12-17 00:48:42 +0100

    x

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Pause-studio-eksamen/', // erstat med dit GitHub repository-navn
   root: '.', // Sæt root til den aktuelle mappe
   build: {
     outDir: 'dist', // Angiv output-mappen
