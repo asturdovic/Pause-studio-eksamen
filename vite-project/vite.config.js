445824445817a072de7b86246329e6a848dd9e3e
astridovic <148537371+asturdovic@users.noreply.github.com>
2024-12-16 23:20:46 +0100

    k

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: 'src', // this should point to your source folder where index.html is located
   build: {
-    outDir: 'dist', // Output-mappen
+    outDir: '../dist', // this specifies the output directory for the build
+    rollupOptions: {
+      input: 'src/index.html', // this should point to your index.html
+    },
   },
-})
+  plugins: [react()],
+});
