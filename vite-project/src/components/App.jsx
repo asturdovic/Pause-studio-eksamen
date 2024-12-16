d5730016e4ef48226f7f7a488a06f7599157f7df
xxlindatrinh <145966260+xxlindatrinh@users.noreply.github.com>
2024-12-16 11:31:12 +0100

    Update App.jsx

--- vite-project/src/components/App.jsx
+++ vite-project/src/components/App.jsx
@@ -12,6 +12,13 @@ import Onboarding from "./Onboarding";
 import Floating from "./Floating";
 import Booket from "./Booket";
 
+useEffect(() => {
+  if (!hasLoaded) {
+    setHasLoaded(true); // Set to true so loading screen only shows once
+    navigate("/Login"); // Redirect to loading screen on first load
+  }
+}, [hasLoaded, navigate]);
+
 function App() {
   return (
     <BrowserRouter>
