5f5a8a804e15e8f380581923532dc9b7e5158111
vohejl <148538744+vohejl@users.noreply.github.com>
2024-12-16 11:12:23 +0100

    opdatering

--- vite-project/src/components/Forside.jsx
+++ vite-project/src/components/Forside.jsx
@@ -2,20 +2,16 @@ import React from "react";
 import "../css/Forside.css";
 
 function Forside() {
-  // Tilføj handleBookingClick-funktionen
-  const handleBookingClick = () => {
-    console.log("Book tid knap klikket!");
-  };
-
   return (
     <div className="forside-container">
       {/* Velkomstsektion */}
       <div className="forside-welcome-section">
         <h1 className="forside-welcome-text">Velkommen Sascha,</h1>
         <p className="forside-pause-text">Tid til at tage en pause?</p>
-        <button className="forside-book-button" onClick={handleBookingClick}>
+        {/* Book tid-knap med href */}
+        <a href="/booking" className="forside-book-button">
           Book tid
-        </button>
+        </a>
       </div>
 
       {/* Belønningssektion */}
@@ -36,26 +32,35 @@ function Forside() {
       <div className="forside-services-wrapper">
         <h3 className="forside-services-title">SERVICES</h3>
         <div className="forside-services-section">
-          <img
-            src="/public/img/service1.png"
-            alt="Massage"
-            className="forside-service-image service-image-1"
-          />
-          <img
-            src="/public/img/service2.png"
-            alt="Sauna"
-            className="forside-service-image service-image-2"
-          />
-          <img
-            src="/public/img/service3.png"
-            alt="Floating"
-            className="forside-service-image service-image-3"
-          />
-          <img
-            src="/public/img/service4.png"
-            alt="Sauna"
-            className="forside-service-image service-image-4"
-          />
+          {/* Klikbare billeder med href til /booking */}
+          <a href="/booking">
+            <img
+              src="/public/img/service1.png"
+              alt="Massage"
+              className="forside-service-image service-image-1"
+            />
+          </a>
+          <a href="/booking">
+            <img
+              src="/public/img/service2.png"
+              alt="Sauna"
+              className="forside-service-image service-image-2"
+            />
+          </a>
+          <a href="/booking">
+            <img
+              src="/public/img/service3.png"
+              alt="Floating"
+              className="forside-service-image service-image-3"
+            />
+          </a>
+          <a href="/booking">
+            <img
+              src="/public/img/service4.png"
+              alt="Sauna"
+              className="forside-service-image service-image-4"
+            />
+          </a>
         </div>
       </div>
     </div>
