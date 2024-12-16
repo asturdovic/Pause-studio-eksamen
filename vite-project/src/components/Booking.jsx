ea50f94782fe2f746c5110494dfe9ea3780868de
xxlindatrinh <145966260+xxlindatrinh@users.noreply.github.com>
2024-12-16 11:13:22 +0100

    floating

--- vite-project/src/components/Booking.jsx
+++ vite-project/src/components/Booking.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import '/src/css/Booking.css';
 
 function Booking() {
-  const [selectedOption, setSelectedOption] = useState(null);
+  // Initialize selectedOption with "Enkelt session" as default
+  const [selectedOption, setSelectedOption] = useState('Enkelt session');
   const navigate = useNavigate();
 
   // Handle item click event to navigate to the page
@@ -45,23 +46,23 @@ function Booking() {
           {selectedOption === 'Enkelt session' && (
             <>
               <div className="option-item" onClick={() => handleSelectOption('Floating')}>
-              <p>Floating</p>
+                <p>Floating</p>
                 <img src="/public/img/floating.png" alt="Floating" />
               </div>
               <div className="option-item" onClick={() => handleSelectOption('Infrared sauna')}>
-              <p>Infrarød sauna</p>
+                <p>Infrarød sauna</p>
                 <img src="/public/img/infrarød.png" alt="Infrared sauna" />
               </div>
               <div className="option-item" onClick={() => handleSelectOption('Recovery boots')}>
-              <p>Recovery boots</p>
+                <p>Recovery boots</p>
                 <img src="/public/img/recoveryboots.png" alt="Recovery boots" />
               </div>
               <div className="option-item" onClick={() => handleSelectOption('Cryosauna')}>
-              <p>Cryosauna</p>
+                <p>Cryosauna</p>
                 <img src="/public/img/cryosauna.png" alt="Cryosauna" />
               </div>
               <div className="option-item" onClick={() => handleSelectOption('Lysbehandling')}>
-              <p>Lysbehandling</p>
+                <p>Lysbehandling</p>
                 <img src="/public/img/lysbehandling.png" alt="Lysbehandling" />
               </div>
             </>
@@ -70,12 +71,20 @@ function Booking() {
           {selectedOption === 'Wellnesspakke' && (
             <>
               <div className="option-item" onClick={() => handleSelectOption('Massage')}>
+              <p>PAUSE de luxe</p>
                 <img src="/public/img/deluxe.png" alt="Billede af en drikkevare der bliver hældt op" />
-                <p>PAUSE de luxe</p>
               </div>
-              <div className="option-item" onClick={() => handleSelectOption('Aromatherapy')}>
-                <img src="/public/img/aromatherapy.png" alt="Aromatherapy" />
-                <p>Aromatherapy</p>
+              <div className="option-item" onClick={() => handleSelectOption('Par der sidder og hygger')}>
+              <p>Stor pause</p>
+                <img src="/public/img/storpause.png" alt="" />
+              </div>
+              <div className="option-item" onClick={() => handleSelectOption('Privat floating rum')}>
+              <p>Privat pause</p>
+                <img src="/public/img/privatpause.png" alt="Aromatherapy" />
+              </div>
+              <div className="option-item" onClick={() => handleSelectOption('Billede af kvinde i kåbe der pumper håndcreme i hånden')}>
+              <p>Lille pause</p>
+                <img src="/public/img/lillepause.png" alt="Aromatherapy" />
               </div>
               {/* Add more wellness pack items as needed */}
             </>
