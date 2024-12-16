bb91ac1281f0955e3dd194053b8d6075ac989e4d
astridovic <148537371+asturdovic@users.noreply.github.com>
2024-12-16 23:18:46 +0100

    k

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./css/index.css"; // Opdateret sti til index.css
-import App from "./src/components/App"; // Opdateret sti til App.jsx
+import App from "./components/App"; // Opdateret sti til App.jsx
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
