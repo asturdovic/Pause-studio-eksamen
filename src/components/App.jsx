d85866084ed588a77ce49848a84f839571838e02
astridovic <148537371+asturdovic@users.noreply.github.com>
2024-12-17 17:14:26 +0100

    <33

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Menubar from "./Menubar"; // Import Menubar component
 import Forside from "./Forside";
 import Booking from "./Booking";
 import Login from "./Login";
@@ -10,18 +11,25 @@ import Floating from "./Floating";
 import Booket from "./Booket";
 
 function App() {
+  const location = useLocation(); // Get the current location (route)
+
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/Forside" element={<Forside />} />
-      <Route path="/Booking" element={<Booking />} />
-      <Route path="/Profil" element={<Profil />} />
-      <Route path="/Medlemsfordele" element={<Medlemsfordele />} />
-      <Route path="/Onboarding" element={<Onboarding />} />
-      <Route path="/Floating" element={<Floating />} />
-      <Route path="/Booket" element={<Booket />} />
-      <Route path="/Program" element={<Program />} />
-    </Routes>
+    <>
+      {/* Conditionally render Menubar, hide it on the login page and onboarding page */}
+      {location.pathname !== "/" && location.pathname !== "/Onboarding" && <Menubar />}  {/* Menubar hidden on / and /Onboarding */}
+      
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Forside" element={<Forside />} />
+        <Route path="/Booking" element={<Booking />} />
+        <Route path="/Profil" element={<Profil />} />
+        <Route path="/Medlemsfordele" element={<Medlemsfordele />} />
+        <Route path="/Onboarding" element={<Onboarding />} />
+        <Route path="/Floating" element={<Floating />} />
+        <Route path="/Booket" element={<Booket />} />
+        <Route path="/Program" element={<Program />} />
+      </Routes>
+    </>
   );
 }
 
