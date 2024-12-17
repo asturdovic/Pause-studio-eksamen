9871a477fb0c254e10e461a02d89b2185282833e
astridovic <148537371+asturdovic@users.noreply.github.com>
2024-12-17 17:02:10 +0100

    <33

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";  // Importer BrowserRouter
 import "./css/index.css"; // Opdateret sti til index.css
 import App from "./components/App"; // Opdateret sti til App.jsx
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Pause-studio-eksamen/"}>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
