db6e2e73c2fc8da91bf690d538142c1b28a82158
astridovic <148537371+asturdovic@users.noreply.github.com>
2024-12-17 17:06:21 +0100

    <333

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import Forside from "./Forside";
 import Booking from "./Booking";
 import Login from "./Login";
 import Profil from "./Profil";
-import Menubar from "./Menubar";
 import Medlemsfordele from "./Medlemsfordele";
 import Program from "./Program";
 import Onboarding from "./Onboarding";
@@ -14,71 +11,17 @@ import Booket from "./Booket";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Standard route (path="/") er ændret til Login */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Login />
-            </>
-          }
-        />
-        <Route
-          path="/Forside"
-          element={
-            <>
-              <Forside />
-              <Menubar />
-            </>
-          }
-        />
-        <Route
-          path="/Booking"
-          element={
-            <>
-
-              <Booking />
-              <Menubar />
-
-              <Login />
- main
-            </>
-          }
-        />
-        <Route
-          path="/Profil"
-          element={
-            <>
-              <Profil />
-              <Menubar />
-            </>
-          }
-        />
-        <Route
-          path="/Medlemsfordele"
-          element={
-            <>
-              <Medlemsfordele />
-              <Menubar />
-            </>
-          }
-        />
-        <Route path="/Onboarding" element={<Onboarding />} />
-        <Route path="/Floating" element={<Floating />} />
-        <Route path="/Booket" element={<Booket />} />
-        <Route
-          path="/Program"
-          element={
-            <>
-              <Program />
-              <Menubar />
-            </>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/Forside" element={<Forside />} />
+      <Route path="/Booking" element={<Booking />} />
+      <Route path="/Profil" element={<Profil />} />
+      <Route path="/Medlemsfordele" element={<Medlemsfordele />} />
+      <Route path="/Onboarding" element={<Onboarding />} />
+      <Route path="/Floating" element={<Floating />} />
+      <Route path="/Booket" element={<Booket />} />
+      <Route path="/Program" element={<Program />} />
+    </Routes>
   );
 }
 
